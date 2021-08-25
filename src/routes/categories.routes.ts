70fb79feef1dd623770817f8297c26aeaa9ee0b9
Yuri Braskulki <yuri.simoes@waac.com.br>
2021-08-25 16:58:45 -0300

    first commit

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+
+import { CategoriesRepository } from '../repositories/categories.repository';
+import { CreateCategoryService } from '../services/create-category.service';
+
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
+
+categoriesRoutes.post('/', (request, response) => {
+    try {
+        const { name, description } = request.body;
+
+        const createCategoryService = new CreateCategoryService(categoriesRepository);
+        createCategoryService.execute({ name, description });
+
+        return response.status(201).send();
+    } catch (err) {
+        return response.status(400).json({ error: err.message });
+    }
+});
+
+categoriesRoutes.get('/', (request, response) => {
+    const categories = categoriesRepository.list();
+    return response.json(categories);
+});
+
+export { categoriesRoutes };
---
+++ src/services/create-category.service.ts
@@ -0,0 +1,22 @@
+import { ICategoriesRepository } from '../repositories/icategories.repository';
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+class CreateCategoryService {
+    constructor(private categoriesRepository: ICategoriesRepository) { } //eslint-disable-line
+
+    execute({ name, description }: IRequest): void {
+        const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+        if (categoryAlreadyExists) {
+            throw new Error('Category already exists!');
+        }
+
+        this.categoriesRepository.create({ name, description });
+    }
+}
+
+export { CreateCategoryService };
